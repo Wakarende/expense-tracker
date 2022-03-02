0a2dbc965480635179de863a3d3161818167a95c
Joy Kirii <joykirii@gmail.com>
2022-03-02 10:05:57 +0300

    add expenses component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
+
+import Expenses from "./components/Expenses";
 
 function App() {
-  const expenses = [
+   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -22,31 +23,10 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-
-
   return (
     <div>
       <h2>Hi Joy!</h2>
-      <ExpenseItem 
-      title={expenses[0].title} 
-      amount={expenses[0].amount} 
-      date={expenses[0].date}>
-      </ExpenseItem>
-      <ExpenseItem 
-      title={expenses[1].title} 
-      amount={expenses[1].amount} 
-      date={expenses[1].date}>
-      </ExpenseItem>
-      <ExpenseItem 
-      title={expenses[2].title} 
-      amount={expenses[2].amount} 
-      date={expenses[2].date}>
-      </ExpenseItem>
-       <ExpenseItem 
-      title={expenses[3].title} 
-      amount={expenses[3].amount} 
-      date={expenses[3].date}>
-      </ExpenseItem>
+      <Expenses expenses = {expenses}></Expenses>
     </div>
   );
 }
